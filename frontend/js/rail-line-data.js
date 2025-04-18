83a0956e92a74f10d6408f2853bee91370202bac
shyke50 <tuo60645@temple.edu>
2025-04-18 11:23:26 -0400

    edits

--- frontend/js/rail-line-data.js
+++ frontend/js/rail-line-data.js
@@ -83,7 +83,12 @@ const RAIL_LINE_DATA = {
   NOR: {
     name: "Manayunk/Norristown Line",
     stations: [
-      { lat: 39.9566, lng: -75.1816, station: "30th Street Station" },
+      { lat: 39.9526, lng: -75.1652, station: "30th Street Station" },
+      { lat: 39.9526, lng: -75.1652, station: "Suburban Station" },
+      { lat: 39.9526, lng: -75.1652, station: "Market East Station" },
+      { lat: 39.9813, lng: -75.1498, station: "Temple University Station" },
+      { lat: 39.9813, lng: -75.1498, station: "North Broad Station" },
+      { lat: 40.0319, lng: -75.1508, station: "Wayne Junction Station" },
       { lat: 39.9685, lng: -75.1965, station: "East Falls Station" },
       { lat: 40.0274, lng: -75.2223, station: "Manayunk Station" },
       { lat: 40.0331, lng: -75.2343, station: "Ivy Ridge Station" },
